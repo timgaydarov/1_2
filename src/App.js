6460fd2a5e677cd68a3e719c87fb2ad5dab356ed
timgaydarov <gaidarov_timur@mail.ru>
2022-03-30 11:10:26 +0300

    fixt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
-import ShopItemClass from './components/ShopItemClass'
+import ShopItemClass from './components/ShopItemClass';
 
-function App(item) {
+const item = {
+	brand: 'Tiger of Sweden',
+	title: 'Leonard coat',
+	description: 'Minimalistic coat in cotton-blend',
+	descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+	price: 399,
+	currency: '£'
+}
+
+function App() {
+
 	return (
 		<div className="container">
 			<div className="background-element">
--- src/components/ShopItemClass.js
+++ src/components/ShopItemClass.js
@@ -2,27 +2,20 @@ import React from 'react';
 
 class ShopItemClass extends React.Component {
 
-	item = {
-		brand: 'Tiger of Sweden',
-		title: 'Leonard coat',
-		description: 'Minimalistic coat in cotton-blend',
-		descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-		price: 399,
-		currency: '£'
-	}
-
 	render() {
 
+		const { item } = this.props;
+
 		return (
-			<div class="main-content">
-				<h2>{this.item.brand}</h2>
-				<h1>{this.item.title}</h1>
-				<h3>{this.item.description}</h3>
-				<div class="description">{this.item.descriptionFull}</div>
-				<div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-				<div class="divider"></div>
-				<div class="purchase-info">
-					<div class="price">{this.item.currency}{this.item.price.toFixed(2)}</div>
+			<div className="main-content">
+				<h2>{item.brand}</h2>
+				<h1>{item.title}</h1>
+				<h3>{item.description}</h3>
+				<div className="description">{item.descriptionFull}</div>
+				<div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+				<div className="divider"></div>
+				<div className="purchase-info">
+					<div className="price">{item.currency}{item.price.toFixed(2)}</div>
 					<button>Добавить в корзину</button>
 				</div>
 			</div>
@@ -32,3 +25,4 @@ class ShopItemClass extends React.Component {
 }
 
 export default ShopItemClass;
+
